c7da743e5304d2fcb4358d0c84ba2f1992b1d939
Avinash174 <avinashmagar15@gmail.com>
2023-12-05 20:46:22 +0530

    new changes done

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Home Screen',
+          style: GoogleFonts.poppins(),
+        ),
+        centerTitle: true,
+      ),
+    );
   }
 }
