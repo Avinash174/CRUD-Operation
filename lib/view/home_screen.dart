469b00926318c0216af47a0d12be6f9797c09cb8
Avinash174 <avinashmagar15@gmail.com>
2023-12-06 00:09:42 +0530

    new changes done

--- lib/view/home_screen.dart
+++ lib/view/home_screen.dart
@@ -12,76 +12,81 @@ class HomeScreen extends StatelessWidget {
     UserModel userModel = UserModel();
     final height = MediaQuery.sizeOf(context).height * 1;
     final width = MediaQuery.sizeOf(context).width * 1;
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'Home Screen',
-          style: GoogleFonts.poppins(
-            color: Colors.white,
-            fontSize: 20,
-            fontWeight: FontWeight.w500,
-          ),
-        ),
-        centerTitle: true,
-        actions: [
-          IconButton(
-            onPressed: () {},
-            icon: const Icon(
-              Icons.refresh_rounded,
+    return DefaultTabController(
+      length: 6,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            'Home Screen',
+            style: GoogleFonts.poppins(
               color: Colors.white,
-              size: 25,
+              fontSize: 20,
+              fontWeight: FontWeight.w500,
             ),
           ),
-          SizedBox(
-            width: width * .01,
-          )
-        ],
-      ),
-      body: ListView(
-        children: [
-          SizedBox(
-            height: height,
-            width: width,
-            child: FutureBuilder(
-                future: UserViewModel().fetchUserDetails(),
-                builder: (context, snapshot) {
-                  if (snapshot.connectionState == ConnectionState.waiting) {
-                    return const Center(
-                      child: SpinKitFadingCircle(
-                        color: Colors.blue,
-                      ),
-                    );
-                  }
-                  return ListView.builder(
-                      itemCount: snapshot.data!.data!.length,
-                      itemBuilder: (context, index) {
-                        return ListTile(
-                          leading: CircleAvatar(
-                            child: ClipRRect(
-                              borderRadius:
-                                  const BorderRadius.all(Radius.circular(20)),
-                              child: Image(
-                                image: NetworkImage(
-                                  snapshot.data!.data![index].avatar.toString(),
-                                ),
-                                fit: BoxFit.cover,
-                              ),
-                            ),
-                            // child: Text(
-                            //   snapshot.data!.data![index].id.toString(),
-                            // ),
-                          ),
-                          title: Text(
-                            snapshot.data!.data![index].firstName.toString() +
-                                " " +
-                                " " +
-                                snapshot.data!.data![index].lastName.toString(),
-                          ),
-                        );
-                      });
-                }),
-          )
-        ],
+          centerTitle: true,
+          actions: [
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                Icons.refresh_rounded,
+                color: Colors.white,
+                size: 25,
+              ),
+            ),
+            SizedBox(
+              width: width * .01,
+            )
+          ],
+        ),
+        body: ListView(
+          children: [
+            SizedBox(
+              height: height,
+              width: width,
+              child: FutureBuilder(
+                  future: UserViewModel().fetchUserDetails(),
+                  builder: (context, snapshot) {
+                    if (snapshot.connectionState == ConnectionState.waiting) {
+                      return const Center(
+                        child: SpinKitFadingCircle(
+                          color: Colors.blue,
+                        ),
+                      );
+                    }
+                    return ListView.builder(
+                        itemCount: snapshot.data!.data!.length,
+                        itemBuilder: (context, index) {
+                          return TabBar(tabs: [
+                            Text(
+                              snapshot.data!.data![index].firstName.toString(),
+                            )
+                          ]);
+                          // return ListTile(
+                          //   leading: CircleAvatar(
+                          //     child: ClipRRect(
+                          //       borderRadius:
+                          //           const BorderRadius.all(Radius.circular(20)),
+                          //       child: Image(
+                          //         image: NetworkImage(
+                          //           snapshot.data!.data![index].avatar.toString(),
+                          //         ),
+                          //         fit: BoxFit.cover,
+                          //       ),
+                          //     ),
+                          //   ),
+                          //   title: Text(
+                          //     snapshot.data!.data![index].firstName.toString(),
+                          //   ),
+                          //   subtitle: Text(
+                          //     snapshot.data!.data![index].lastName.toString(),
+                          //   ),
+                          // );
+                        });
+                  }),
+            )
+          ],
+        ),
       ),
     );
   }
