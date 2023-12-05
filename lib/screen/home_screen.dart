279e69263405578c71991cf3df4ea3a0f93029a2
Avinash174 <avinashmagar15@gmail.com>
2023-12-05 20:57:40 +0530

    new changes add

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -6,13 +6,34 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final height = MediaQuery.sizeOf(context).height * 1;
+    final width = MediaQuery.sizeOf(context).width * 1;
     return Scaffold(
       appBar: AppBar(
         title: Text(
           'Home Screen',
-          style: GoogleFonts.poppins(),
+          style: GoogleFonts.poppins(
+            color: Colors.white,
+            fontWeight: FontWeight.w500,
+          ),
         ),
         centerTitle: true,
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(
+              Icons.refresh_rounded,
+              color: Colors.white,
+              size: 30,
+            ),
+          ),
+          SizedBox(
+            width: width * .01,
+          )
+        ],
+      ),
+      body: Column(
+        children: [],
       ),
     );
   }
